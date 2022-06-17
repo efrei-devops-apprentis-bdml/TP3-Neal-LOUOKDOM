51d5768cd74703c9ab6e99817ff7902a3c0ff18e
Neal FOKAM <66922510+Christopher2023@users.noreply.github.com>
2022-06-17 09:33:23 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:slim-buster
+
+COPY . /app/
+
+WORKDIR /app
+
+RUN pip install --no-cache-dir requests==2.27.1 hug==2.6.1
+
+CMD hug -f meteo.py 
---
+++ meteo.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import hug
+
+
+@hug.get('/echo')
+def echo(lat:hug.types.text, lon:hug.types.text):
+
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    url = url + "lat=" + lat + "&lon=" + lon + "&appid=" + str(os.environ.get('API_KEY'))
+    
+    response = requests.get(url).json()
+
+    
+    if response["cod"] == "404":
+        return ("erreur")
+    else:
+        return response
+        
+
+
+hug.API(__name__).http.serve(port=8081)
